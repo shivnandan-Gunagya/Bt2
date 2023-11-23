5d497b55eb3f540e07f014c5a14cc0e235a2ca81
shivnandan-Gunagya <shivnandansoni@gunagya.in>
2023-11-23 10:56:08 +0530

    bt2

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonicModule } from '@ionic/angular';
+import { BleClient } from '@capacitor-community/bluetooth-le';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonicModule],
 })
 export class Tab1Page {
   constructor() {}
+
+  async hello (){
+    await BleClient.initialize();
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalo
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
 
 if (environment.production) {
   enableProdMode();
